5aa96d091e1a4293ba5a94834f3ded0e85c73af1
Gularis <michalv.balko@gmail.com>
2017-05-14 11:41:12 +0200

    heroku mysql credentials

--- config/database.php
+++ config/database.php
@@ -42,11 +42,11 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', '127.0.0.1'),
+            'host' => env('DB_HOST', 'us-cdbr-iron-east-03.cleardb.net'),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'database' => env('DB_DATABASE', 'heroku_89e772416892cba'),
+            'username' => env('DB_USERNAME', 'be429a34c52544'),
+            'password' => env('DB_PASSWORD', 'c8a5a739'),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
             'prefix' => '',
